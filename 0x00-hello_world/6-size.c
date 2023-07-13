c9e9b3f5102e83abb634c7e8d4fc09b1b38d4a79
NyanjauM <nyanjaumurage@gmail.com>
2023-07-13 10:21:15 -0700

    Size is not grandeur, and territory does not make a nation

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,3 +2,14 @@
 /**
  * main - A program that prints the size of various computer types
  *  Return 0 (Success)
+ */
+int main(void)
+{
+	pritntf("Size of char: %lu\n", sizeof(char));
+	pritntf("Size of int: %lu\n", sizeof(int));
+	pritntf("Size of float: %lu\n", sizeof(float));
+	pritntf("Size of long int: %lu\n", sizeof(long int));
+	pritntf("Size of lon long int: %lu\n", sizeof(long long int));
+
+	return 0;
+}
