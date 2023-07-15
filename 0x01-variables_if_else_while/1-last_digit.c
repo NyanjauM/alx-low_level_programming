a3c9c2dcc13ec13a374e70576678645581d98252
NyanjauM <nyanjaumurage@gmail.com>
2023-07-15 01:03:13 -0700

    Positive anything is better than negative nothing

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/*
+ * main - assigns a randm number to variable n and states whether
+ * it is a positive, negative or a zero
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n < 0)
+		printf("%d is negative\n", n);
+	else
+		printf("%d is zero\n", n);
+	return (0);
+}
