7ebbf38d012759a0a319a5e2cde218969d4e8349
NyanjauM <nyanjaumurage@gmail.com>
2023-07-16 00:17:19 -0700

    alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,16 +8,15 @@
  * Main - Entry point
  *
  * Return: Always 0 (Success)
- **/
-/*main prints alphabets*/
+ */
 int main(void)
 {	char l;
-/*lowercase*/
+
 	for (l = 'a'; l <= 'z'; l++)
 	putchar(l);
-/*UPPERCASE*/
 	for (l = 'A'; l <= 'B'; l++)
 	putchar(l);
 	putchar('\n');
+
 	return (0);
 }
