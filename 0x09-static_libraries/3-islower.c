7755bc05e128398156e5757c1f0e3faa3e0e94d8
NyanjauM <nyanjaumurage@gmail.com>
2023-08-04 07:08:57 -0700

    A library is not a luxury but one of the necessities of life

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _islowe - checks for lowercase character
+ * @c: the character to check
+ * Return: 1 if c lowercase, 0 otherwise
+ */
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
