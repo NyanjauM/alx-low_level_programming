fe2799c6208f0359c8261bb5fb218ea3ba4946ed
NyanjauM <nyanjaumurage@gmail.com>
2023-08-06 02:10:32 -0700

    C programming

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - check the code
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+	char al;
+
+for (al = 'a'; al <= 'z'; al++)
+_putchar(al);
+_putchar('\n');
+}
